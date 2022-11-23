3809471161433031fc84657b6930195c9147154b
Nenad Čaklović <nenad.caklovic@reversinglabs.com>
2022-11-24 00:37:32 +0100

    Solution

--- app/app.cpp
+++ app/app.cpp
@@ -1,1 +1,52 @@
 #include "app.h"
+
+uint32_t sum_digits(const std::string& s) {
+    uint32_t n{ 0 };
+    for (auto c : s)
+        if (isdigit(c))
+            n += c - '0';
+    return n;
+}
+
+uint32_t visible_towers(const uint32_t* t, uint32_t n) {
+    auto h = t[0];
+    auto c{ 1 };
+    for (uint32_t i = 1; i < n; ++i) {
+        if (t[i] > h) {
+            h = t[i];
+            ++c;
+        }
+    }
+    return c;
+}
+
+std::string windows::release() const { return "22H2"; }
+std::string windows::build() const { return "10.0.22621.819"; }
+unsigned long long windows::space() const { return 25_GB; }
+
+
+string::string(const char* s) : len(strlen(s)), p(new char[len + 1]) {
+    strcpy(p, s);
+}
+string::string(const string& other) : len(other.len), p(new char[len + 1]) {
+    strcpy(p, other.p);
+}
+string::~string() {
+    delete[] p;
+}
+uint32_t string::length() const {
+    return len;
+}
+string string::rev_sub(uint32_t start, uint32_t length) const {
+    char* arr = new char[length + 1];
+    for (uint32_t i = 0; i < length; ++i)
+        arr[i] = p[start - i - 1];
+    arr[length] = 0;
+    string t(arr);
+    delete[] arr;
+    return t;
+}
+void string::print(std::ostream& os) const {
+    os << p;
+}
+
--- app/app.h
+++ app/app.h
@@ -3,3 +3,37 @@
 #include <string>
 #include <iostream>
 
+inline unsigned long long int operator "" _GB(unsigned long long int n) {
+    return n * 1024 * 1024 * 1024;
+}
+
+uint32_t visible_towers(const uint32_t* t, uint32_t n);
+
+uint32_t sum_digits(const std::string& s);
+
+class operating_system {
+public:
+    virtual std::string release() const = 0;
+    virtual std::string build() const = 0;
+    virtual unsigned long long space() const = 0;
+    virtual ~operating_system() = default;
+};
+
+class windows : public operating_system {
+public:
+    std::string release() const override;
+    std::string build() const override;
+    unsigned long long space() const override;
+};
+
+class string {
+    int len;
+    char* p;
+public:
+    string(const char*);
+    string(const string&);
+    ~string();
+    uint32_t length() const;
+    string rev_sub(uint32_t start, uint32_t length) const;
+    void print(std::ostream& os) const;
+};
