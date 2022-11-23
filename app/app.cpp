002a7704dede451bc51447bfbf450e8787374b0a
Matko <tkoma999@gmail.com>
2022-11-23 18:55:50 +0100

    k01 Matko Stojic

--- app/app.cpp
+++ app/app.cpp
@@ -1,1 +1,40 @@
 #include "app.h"
+#include <string.h>
+
+
+unsigned sum_digits(std::string s){
+    unsigned a=0;
+    for(int i=0;i<s.length();++i)
+       if(isdigit(s[i]))a+= s[i]-'0';
+       
+    
+    return a;
+
+}
+    int operator""_GB(unsigned long long int n){return n;}
+   
+    std::string windows::release()const{return "22H2";}
+    std::string windows::build()const{return "10.0.22621.819";}
+    int windows::space()const{return 25_GB;}
+
+
+    string::string(const char* c):s{new char[size]},size{strlen(c)}{
+        for(int i=0;i<size;++i)s[i]=c[i];
+    }
+    unsigned string::length(){
+        strlen(s);
+    }
+    string::~string(){
+        delete[] s;
+        s=nullptr;
+    }
+    string::string(const string& other):s{new char[size]},size{strlen(other.s)}{
+        for(int i=0;i<size;++i)other.s[i]=s[i];
+    }
+    string& string::rev_sub(int a,int b){
+        for(int i=0;i<b;++i);
+
+
+        return *this;
+        
+    }
--- app/app.h
+++ app/app.h
@@ -3,3 +3,44 @@
 #include <string>
 #include <iostream>
 
+unsigned sum_digits(std::string s);
+
+
+
+
+int operator""_GB(unsigned long long int n);
+class operating_system{
+    
+    public:
+           virtual std::string release()const=0;
+           virtual std::string build()const=0;
+           virtual int space()const=0;
+           virtual ~operating_system()=default;
+
+    
+};
+
+class windows:public operating_system{
+
+        public:
+            std::string release()const override;
+            std::string build()const override;
+            int space()const override;
+
+
+
+};
+
+class string{
+    char* s;
+    int size;
+    public:
+     string(const char* c);
+     string(const string& other);
+     ~string();
+     unsigned length();
+     string& rev_sub(int a,int b);
+     void print(std::ostream& ss);
+
+
+};
