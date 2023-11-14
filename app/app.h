857c1c23b2b221c80c7f67f122fe3b942e176a42
Bogjenero <bornagjenero1@gmail.com>
2023-11-14 11:23:58 +0100

    Rijesen kolokvij

--- app/app.h
+++ app/app.h
@@ -3,3 +3,37 @@
 #include <string>
 #include <iostream>
 
+
+
+unsigned sum_digits(std::string s);
+
+unsigned visible_towers(const uint32_t* niz, int size);
+
+int operator "" _GB(unsigned long long int broj);
+
+
+class operating_system {
+public:
+	virtual std::string release() const  = 0;
+	virtual std::string build() const = 0;
+	virtual int space() const = 0;
+	virtual ~operating_system() = default;
+};
+
+class windows : public operating_system {
+public:
+	std::string release() const ;
+	std::string build() const ;
+	int space() const ;
+};
+
+class string {
+	std::string s1;
+	int size;
+public:
+	string(std::string s);
+	string(const string& s);
+	unsigned length() const;
+	string rev_sub(int start, int duljina) const;
+	void print(std::ostream& s) const;
+};
--- app/main.cpp
+++ app/main.cpp
@@ -2,4 +2,9 @@
 
 int main()
 {
+
+	const string s1("Hrvatska");
+	const string s2 = s1.rev_sub(3, 3);
+	s2.print(std::cout);
+
 }
