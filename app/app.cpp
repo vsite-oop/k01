4c8d527208305626e04c37d6f026274eb8ac0161
DrHrast <petar.huljek@gmail.com>
2023-11-19 23:50:56 +0100

    Resolve kolokvij 1 second attempt.

--- app/app.cpp
+++ app/app.cpp
@@ -1,1 +1,75 @@
 #include "app.h"
+
+uint32_t sum_digits(const std::string& unos) {
+	uint32_t sum = 0;
+	for (char v : unos) {
+		if (isdigit(v)) {
+			sum += v - '0';
+		}
+	}
+	return sum;
+}
+
+uint32_t visible_towers(const uint32_t tornjevi[], int size_of) {
+	uint32_t max = tornjevi[0];
+	uint32_t n_visible = 1;
+	for (uint32_t i = 0; i < size_of; i++) {
+		if (max < tornjevi[i]) {
+			max = tornjevi[i];
+			n_visible++;
+		}
+	}
+	return n_visible;
+}
+
+int operator "" _GB(unsigned long long mem) {
+	return mem;
+}
+
+windows::windows() {};
+
+std::string windows::release() const {
+	return "22H2";
+}
+
+std::string windows::build() const {
+	return "10.0.22621.819";
+}
+
+int windows::space() const {
+	return 25_GB;
+}
+
+string::string(const char* s) {
+	size = strlen(s);
+	vrijednost = new char[size + 1];
+	strcpy(vrijednost, s);
+}
+
+string::string(const string& other) {
+	size = other.size;
+	vrijednost = new char[size + 1];
+	strcpy(vrijednost, other.vrijednost);
+}
+
+string::~string() {
+	delete[] vrijednost;
+}
+
+uint32_t string::length() const{
+	return size;
+}
+
+string string::rev_sub(int start, int len) const{
+	char* new_string = new char[len + 1];
+	for (int i = 0; i < len; i++) {
+		new_string[i] = vrijednost[start - i - 1];
+	}
+	new_string[len] = '\0';
+
+	return string(new_string);
+}
+
+void string::print(std::ostream& os) const{
+	os << vrijednost;
+}
--- app/app.h
+++ app/app.h
@@ -3,3 +3,39 @@
 #include <string>
 #include <iostream>
 
+uint32_t sum_digits(const std::string& unos);
+
+uint32_t visible_towers(const uint32_t tornjevi[], int size_of);
+
+int operator "" _GB(unsigned long long mem);
+
+class operating_system {
+public:
+	virtual std::string release() const = 0;
+	virtual std::string build() const = 0;
+	virtual int space() const = 0;
+	virtual ~operating_system() = default;
+};
+
+class windows : public operating_system {
+public:
+	windows();
+	std::string release() const;
+	std::string build() const;
+	int space() const;
+};
+
+class string {
+private:
+	uint32_t size;
+	char* vrijednost;
+
+public:
+	string(const char* s);
+	string(const string&);
+	~string();
+
+	uint32_t length() const;
+	string rev_sub(int start, int len) const;
+	void print(std::ostream& os) const;
+};
